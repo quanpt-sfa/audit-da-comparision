3743bc78a1f6548ac24fe5e325bac4aedde24d78
Quan Truong Pham <phamtruongquan@iuh.edu.vn>
2026-07-20 00:44:33 +0700

    Use dual common-sample validation

--- src/audit_da/diag_cfs_proxy_validation.py
+++ src/audit_da/diag_cfs_proxy_validation.py
@@ -1,6 +1,11 @@
 from .cfs_item_map import classify_cfs_item, compile_item_rules, inventory_and_line_items
-from .cfs_proxy_validate import rolling_expected_cfo_proxies, validate_proxy_predictions
+from .cfs_proxy_validate import rolling_expected_cfo_proxies
+from .cfs_proxy_validate_samples import validate_proxy_predictions_dual_common
 from .cfs_line_reconcile import line_item_reconciliation
+
+
+def validate_proxy_predictions(predictions, observed_cases, settings):
+    return validate_proxy_predictions_dual_common(predictions, observed_cases, settings)
 
 
 def run_cfs_shifting_validation(panel, observed_cases, line_item_panel, settings):
