a23858038fb841b995a6fb30708fde3761ecff75
Quan Truong Pham <phamtruongquan@iuh.edu.vn>
2026-07-19 16:02:17 +0700

    Expose next-diagnostics API

---
+++ src/audit_da/next_diagnostics.py
@@ -0,0 +1,7 @@
+from .diag_common import paired_panel, trimmed_mean, write_tables
+from .diag_tails import sign_state, sign_transition_tables, ta_source_audit, tail_case_tables
+from .diag_placebo import directional_placebo
+from .diag_calibration import rolling_calibration
+from .diag_discordance import family_discordance
+
+__all__ = ["paired_panel","trimmed_mean","write_tables","sign_state","sign_transition_tables","ta_source_audit","tail_case_tables","directional_placebo","rolling_calibration","family_discordance"]
